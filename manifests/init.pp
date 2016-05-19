996beb8420f23919199635205706684dc11edd99
William Hurley <whurley@forumone.com>
2016-05-19 16:55:38 -0400

    Changing ownership of vagrant-cachier directory

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,8 +14,8 @@ class forumone ($ports = [80, 443, 8080, 8081, 18983, 8983, 3306, 13306, 1080, 4
   # Vagrant cachier directory
   file { "/tmp/vagrant-cache":
     ensure  => "directory",
-    owner   => "vagrant",
-    group   => "vagrant"
+    owner   => $::puppet_user,
+    group   => $::puppet_group
   }
 
   file { "/home/vagrant/.bashrc":

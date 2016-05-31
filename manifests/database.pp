b3bcc8dec61c336efe27b925373a5d035698219b
William Hurley <william@saplingstudio.com>
2016-05-31 15:43:47 -0400

    NSS version (#43)
    * Forcing update to NSS package

--- manifests/database.pp
+++ manifests/database.pp
@@ -6,15 +6,26 @@ class forumone::database (
     'mysqld/log_bin' => 'absent'
   }
 ) {
-  file { '/etc/mysql': ensure => 'directory', } ->
-  file { '/etc/mysql/conf.d': ensure => 'directory' } ->
+  file { '/etc/mysql': 
+    ensure => 'directory'
+  }
+
+  file { '/etc/mysql/conf.d': 
+    ensure => 'directory' 
+  }
+
+  package { 'nss': 
+    ensure  => 'latest',
+    before  => Class['percona::preinstall']
+  }
+
   class { 'percona':
     server             => $server,
     percona_version    => $version,
     manage_repo        => $manage_repo,
     config_include_dir => '/etc/mysql/conf.d',
     configuration      => $configuration,
-    require            => File['/etc/mysql/conf.d']
+    require            => [ File['/etc/mysql/conf.d'], Package['nss'] ]
   }
   
   create_resources('percona::conf', hiera_hash('percona::conf', {

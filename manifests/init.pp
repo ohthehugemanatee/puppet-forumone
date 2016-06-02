ec38949334f59293a878aba40f8937cd198f1811
William Hurley <whurley@forumone.com>
2016-06-02 10:07:50 -0400

    Changing name of group

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,7 @@ class forumone ($ports = [80, 443, 8080, 8081, 18983, 8983, 3306, 13306, 1080, 4
   user { 'vagrant': 
     ensure  => present,
     uid     => $::puppet_user,
-    require => Group[$::puppet_group]  
+    require => Group['vagrant']  
   }
   
   file { "/vagrant":   

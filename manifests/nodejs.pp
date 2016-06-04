307259bee3ec2519c710c9f8884edb9cd143348a
William Hurley <william@saplingstudio.com>
2016-06-03 22:17:16 -0400

    Gcc 48 (#46)
    * Installing g++ 4.8 through software collections

--- manifests/nodejs.pp
+++ manifests/nodejs.pp
@@ -4,6 +4,17 @@ class forumone::nodejs ($modules = ["grunt-cli", "bower"], $version = 'v4.4.1') 
     make_install => false,
   }
 
+  yumrepo { "devtools2":
+    baseurl  => 'http://people.centos.org/tru/devtools-2/$releasever/$basearch/RPMS',
+    descr    => "testing 2 devtools for CentOS $releasever",
+    enabled  => 1,
+    gpgcheck => 0
+  }
+  
+  package { [ 'devtoolset-2-gcc', 'devtoolset-2-binutils', 'devtoolset-2-gcc-c++' ]: 
+    require  => Yumrepo['devtools2']
+  }
+  
   package { $modules:
     ensure   => present,
     provider => 'npm',
